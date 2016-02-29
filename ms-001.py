83f65b680730c905a6ca32eb25f87b36cd43d75d
ChromCode <erpoels@gmail.com>
2016-02-28 17:04:43 -0800

    Indent Error
    Just a Small indent error fix

--- ms-001.py
+++ ms-001.py
@@ -3,10 +3,10 @@ from random import randint
 debug = False
 
 def populate_bomb_array(bomb_array, max, bomb_count):
-	while len(bomb_array) < bomb_count:
-		bomb_pos = randint(0, max-1)
-		if bomb_pos not in bomb_array:
-			bomb_array.append(bomb_pos)
+    while len(bomb_array) < bomb_count:
+	bomb_pos = randint(0, max-1)
+	if bomb_pos not in bomb_array:
+	    bomb_array.append(bomb_pos)
 
 def test_x(row, column):
     test_x = randint(0, max-1)
@@ -23,8 +23,9 @@ def fetch_bomb_count(bomb_array, test_x, test_y):
             try:
                 if bomb_array[x][y] == -1:
                     bomb_count += 1
-            except IndexError:
-                if debug: print "IndexError, do nothing"
+            except IndexError: #Ignore the Index error if the cell doesn't exist
+                if debug: print "IndexError, do nothing" #print a message if debug is on
+                #Otherwise do nothing
     return bomb_count
 
 def replace_bomb_count(canvas, bomb_array):
